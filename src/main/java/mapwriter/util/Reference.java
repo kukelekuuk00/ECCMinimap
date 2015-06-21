6aa99f0f0707b9159f2baeb8f0b408d7c4a40d66
JamieSinn <james.sinn@sinndevelopment.com>
2015-06-20 20:15:33 -0400

    ECC Minimap

--- src/main/java/mapwriter/util/Reference.java
+++ src/main/java/mapwriter/util/Reference.java
@@ -4,8 +4,8 @@ import java.util.regex.Pattern;
 
 public final class Reference 
 {
-	public static final String MOD_ID = "MapWriter";
-	public static final String MOD_NAME = "MapWriter";
+	public static final String MOD_ID = "ECCMinimap";
+	public static final String MOD_NAME = "ECCMinimap";
 	public static final String VERSION = "@MOD_VERSION@";
 	public static final String MOD_GUIFACTORY_CLASS = "mapwriter.gui.ModGuiFactoryHandler";
 	public static final String CLIENT_PROXY_CLASS = "mapwriter.forge.ClientProxy";
